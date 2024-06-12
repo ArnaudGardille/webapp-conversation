7a713dae783106d8687a8cd2f8040ad368f7c8c4
ArnaudGardille <arnaud.gardille@gmail.com>
2024-06-12 23:30:34 +0200

    to french

--- i18n/i18next-config.ts
+++ i18n/i18next-config.ts
@@ -2,14 +2,25 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import commonEn from './lang/common.en'
+import commonFr from './lang/common.fr'
 import commonZh from './lang/common.zh'
 import appEn from './lang/app.en'
+import appFr from './lang/app.fr'
 import appZh from './lang/app.zh'
 import toolsEn from './lang/tools.en'
+import toolsFr from './lang/tools.fr'
 import toolsZh from './lang/tools.zh'
 import type { Locale } from '.'
 
 const resources = {
+  'fr': {
+    translation: {
+      common: commonFr,
+      app: appFr,
+      // tools
+      tools: toolsFr,
+    },
+  },
   'en': {
     translation: {
       common: commonEn,
---
+++ i18n/lang/common.fr.ts
@@ -0,0 +1,33 @@
+const translation = {
+  api: {
+    success: "Succès",
+    saved: "Enregistré",
+    create: "Créé",
+  },
+  operation: {
+    confirm: "Confirmer",
+    cancel: "Annuler",
+    clear: "Effacer",
+    save: "Sauvegarder",
+    edit: "Modifier",
+    refresh: "Redémarrer",
+    search: "Rechercher",
+    send: "Envoyer",
+    lineBreak: "Saut de ligne",
+    like: "aimer",
+    dislike: "ne pas aimer",
+    ok: "OK",
+  },
+  imageUploader: {
+    uploadFromComputer: "Télécharger depuis l'ordinateur",
+    uploadFromComputerReadError: "Échec de la lecture de l'image, veuillez réessayer.",
+    uploadFromComputerUploadError: "Échec du téléchargement de l'image, veuillez réessayer.",
+    uploadFromComputerLimit: "Les images téléchargées ne peuvent pas dépasser {{size}} Mo",
+    pasteImageLink: "Coller le lien de l'image",
+    pasteImageLinkInputPlaceholder: "Coller le lien de l'image ici",
+    pasteImageLinkInvalid: "Lien d'image invalide",
+    imageUpload: "Téléchargement d'image",
+  },
+}
+
+export default translation
